7663464f4fbb614d1508587465a77ea03b7cdb4a
Andreloren <andre.lorente83@gmail.com>
2022-12-16 19:36:18 -0300

    ajustes

--- src/index.ts
+++ src/index.ts
@@ -1,166 +1,189 @@
-import express, { Express, Request, Response, NextFunction } from 'express'
-import cors from 'cors'
-import { v4 } from 'uuid'
-import { retornaIndiceDoGrowdever, verificaSeHaGrowdevers } from './middlewares'
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import { v4 } from "uuid";
+import {
+  retornaIndiceDoGrowdever,
+  verificaSeHaGrowdevers,
+} from "./middlewares";
 
-const server = express()
+const server = express();
 
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
 interface IGrowdever {
-    id: string,
-    nome: string,
-    curso: string,
-    skills?: string[]
+  id: string;
+  nome: string;
+  curso: string;
+  skills?: string[];
 }
 
 export interface RespostaPadrao {
-    sucesso: boolean,
-    mensagem: string,
-    dados: any
+  sucesso: boolean;
+  mensagem: string;
+  dados: any;
 }
 
 class Growdever implements IGrowdever {
-    id: string
-    constructor(public nome: string, public curso: string, public skills?: string[]) {
-        this.id = v4();
-        this.nome = nome;
-        this.curso = curso;
-        this.skills = skills || [];
-    }
+  id: string;
+  constructor(
+    public nome: string,
+    public curso: string,
+    public skills?: string[]
+  ) {
+    this.id = v4();
+    this.nome = nome;
+    this.curso = curso;
+    this.skills = skills || [];
+  }
 }
 
-export let Growdevers: Growdever[] = []
+export let Growdevers: Growdever[] = [];
 
 // ============== LISTAR GROWDEVERS
-server.get('/growdevers', verificaSeHaGrowdevers, (req: Request, res: Response) => {
-    // if(Growdevers.length === 0) return res.status(404).json({
-    //     sucesso: false,
-    //     mensagem: "Não foi localizado nenhum growdever",
-    //     dados: null
-    // } as RespostaPadrao)
-
+server.get(
+  "/growdevers",
+  verificaSeHaGrowdevers,
+  (req: Request, res: Response) => {
     res.status(200).json({
-        sucesso: true,
-        mensagem: "Growdevers localizados",
-        dados: Growdevers
-    })
-})
-
+      sucesso: true,
+      mensagem: "Growdevers localizados",
+      dados: Growdevers,
+    });
+  }
+);
 
 // ============== NOVO GROWDEVER
-server.post('/growdevers', (req: Request, res: Response) => {
-    const { nome, curso, skills } = req.body
+server.post("/growdevers", (req: Request, res: Response) => {
+  const { nome, curso, skills } = req.body;
 
-    if(!nome) return res.status(400).json({
-        sucesso: false,
-        mensagem: "Falta nome",
-        dados: null
-    } as RespostaPadrao)
+  if (!nome)
+    return res.status(400).json({
+      sucesso: false,
+      mensagem: "Falta nome",
+      dados: null,
+    } as RespostaPadrao);
 
-    if(!curso) return res.status(400).json({
-        sucesso: false,
-        mensagem: "Falta curso",
-        dados: null
-    } as RespostaPadrao)
+  if (!curso)
+    return res.status(400).json({
+      sucesso: false,
+      mensagem: "Falta curso",
+      dados: null,
+    } as RespostaPadrao);
 
-    const novoGrowdever = new Growdever(nome, curso, skills)
+  const novoGrowdever = new Growdever(nome, curso, skills);
 
-    Growdevers.push(novoGrowdever)
+  Growdevers.push(novoGrowdever);
 
-    console.log(Growdevers)
+  console.log(Growdevers);
 
-    res.status(201).json({
-        sucesso: true,
-        mensagem: "Growdever Criado",
-        dados: novoGrowdever
-    })
-})
-
+  res.status(201).json({
+    sucesso: true,
+    mensagem: "Growdever Criado",
+    dados: novoGrowdever,
+  });
+});
 
 // ============== ALTERAR GROWDEVER
-server.put('/growdevers/:id', retornaIndiceDoGrowdever, (req: Request, res: Response) => {
-    const { nome, curso, indice } = req.body
+server.put(
+  "/growdevers/:id",
+  retornaIndiceDoGrowdever,
+  (req: Request, res: Response) => {
+    const { nome, curso, indice } = req.body;
 
-    if(nome) Growdevers[indice].nome = nome.toString();
-    if(curso) Growdevers[indice].curso = curso.toString();
+    if (nome) Growdevers[indice].nome = nome.toString();
+    if (curso) Growdevers[indice].curso = curso.toString();
 
-    if(!curso && !nome) return res.status(404).json({
+    if (!curso && !nome)
+      return res.status(404).json({
         sucesso: false,
         mensagem: "Não foram informadas alterações",
-        dados: null
-    } as RespostaPadrao)
+        dados: null,
+      } as RespostaPadrao);
 
     return res.status(200).json({
-        sucesso: true,
-        mensagem: "Growdever alterado",
-        dados: Growdevers[indice]
-    } as RespostaPadrao)
-
-})
-
+      sucesso: true,
+      mensagem: "Growdever alterado",
+      dados: Growdevers[indice],
+    } as RespostaPadrao);
+  }
+);
 
 // ============== CRIAR SKILL
-server.post('/growdevers/:id', retornaIndiceDoGrowdever, (req: Request, res: Response) => {
-    const { indice, skill } = req.body
+server.post(
+  "/growdevers/:id",
+  retornaIndiceDoGrowdever,
+  (req: Request, res: Response) => {
+    const { indice, skill } = req.body;
 
-    if(!skill) return res.status(404).json({
+    if (!skill)
+      return res.status(404).json({
         sucesso: false,
         mensagem: "Skill não informada",
-        dados: null
-    } as RespostaPadrao)
+        dados: null,
+      } as RespostaPadrao);
 
-    Growdevers[indice].skills?.push(skill.toString())
+    Growdevers[indice].skills?.push(skill.toString());
 
     res.status(201).json({
-        sucesso: true,
-        mensagem: "Skill Criada",
-        dados: Growdevers[indice]
-    })
-})
+      sucesso: true,
+      mensagem: "Skill Criada",
+      dados: Growdevers[indice],
+    });
+  }
+);
 
 // ============== DELETAR SKILL
-server.delete('/growdevers/:id/:skill', retornaIndiceDoGrowdever, (req: Request, res: Response) => {
-    const { skill } = req.params
-    const { indice } = req.body
+server.delete(
+  "/growdevers/:id/:skill",
+  retornaIndiceDoGrowdever,
+  (req: Request, res: Response) => {
+    const { skill } = req.params;
+    const { indice } = req.body;
 
-    if(!skill) return res.status(404).json({
+    if (!skill)
+      return res.status(404).json({
         sucesso: false,
         mensagem: "Não foi informada Skill a ser apagada",
-        dados: null
-    } as RespostaPadrao)
+        dados: null,
+      } as RespostaPadrao);
 
-    const posicaoSkill = Growdevers[indice].skills!.findIndex(sk => sk.toLowerCase() === skill.toLowerCase())
+    const posicaoSkill = Growdevers[indice].skills!.findIndex(
+      (sk) => sk.toLowerCase() === skill.toLowerCase()
+    );
 
-    if(posicaoSkill === -1) return res.status(404).json({
+    if (posicaoSkill === -1)
+      return res.status(404).json({
         sucesso: false,
         mensagem: "Skill informada não foi localizada",
-        dados: null
-    } as RespostaPadrao)
+        dados: null,
+      } as RespostaPadrao);
 
-    Growdevers[indice].skills!.splice(posicaoSkill, 1)
-
-    res.status(200).json({
-        sucesso: true,
-        mensagem: "Skill Removida",
-        dados: Growdevers[indice]
-    } as RespostaPadrao)
-})
-
-
-server.delete('/growdevers/:id', retornaIndiceDoGrowdever, (req: Request, res: Response) => {
-    const { indice } = req.body
-    const gdExcluido = Growdevers[indice]
-
-    Growdevers.splice(indice, 1)
+    Growdevers[indice].skills!.splice(posicaoSkill, 1);
 
     res.status(200).json({
-        sucesso: true,
-        mensagem: "Growdever Excluído",
-        dados: gdExcluido
-    } as RespostaPadrao)
+      sucesso: true,
+      mensagem: "Skill Removida",
+      dados: Growdevers[indice],
+    } as RespostaPadrao);
+  }
+);
 
-})
+server.delete(
+  "/growdevers/:id",
+  retornaIndiceDoGrowdever,
+  (req: Request, res: Response) => {
+    const { indice } = req.body;
+    const gdExcluido = Growdevers[indice];
 
-server.listen(3000, () => console.log('Rodando'))
+    Growdevers.splice(indice, 1);
+
+    res.status(200).json({
+      sucesso: true,
+      mensagem: "Growdever Excluído",
+      dados: gdExcluido,
+    } as RespostaPadrao);
+  }
+);
+
+server.listen(3000, () => console.log("Rodando"));
--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,30 +1,39 @@
-import express, { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-import { Growdevers, RespostaPadrao, } from ".";
+import { Growdevers, RespostaPadrao } from ".";
 
 // ======== verificação para o GET
-export function verificaSeHaGrowdevers(req: Request, res: Response, next: NextFunction) {
+export function verificaSeHaGrowdevers(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   if (Growdevers.length === 0)
     return res.status(404).json({
       sucesso: false,
       mensagem: "Não foi localizado nenhum growdever",
       dados: null,
     } as RespostaPadrao);
-    next()
+  next();
 }
 
 // ======== retorna indice
-export function retornaIndiceDoGrowdever(req: Request, res: Response, next: NextFunction){
-    const { id } = req.params
-    const { nome, curso, skill } = req.body
-    const indice = Growdevers.findIndex((gd) => gd.id === id)
+export function retornaIndiceDoGrowdever(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { id } = req.params;
+  const { nome, curso, skill } = req.body;
+  const indice = Growdevers.findIndex((gd) => gd.id === id);
 
-    if(indice === -1) return res.status(404).json({
-        sucesso: false,
-        mensagem: "Growdever não localizado",
-        dados: null
-    } as RespostaPadrao)
+  if (indice === -1)
+    return res.status(404).json({
+      sucesso: false,
+      mensagem: "Growdever não localizado",
+      dados: null,
+    } as RespostaPadrao);
 
-    req.body = { nome, curso, indice, skill }
-    next()
+  req.body = { nome, curso, indice, skill };
+  next();
 }
